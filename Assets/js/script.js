29fd1808e56544c8fd114f8c74b2933bb5aff970
Javid <javidia@code.edu.az>
2021-11-15 17:54:14 +0400

    Initial commit

---
+++ Assets/js/script.js
@@ -0,0 +1,114 @@
+// alert("Hello P223")
+// document.writeln("Hello P223");
+
+
+
+
+// let firstname = "Hasan";
+
+// firstname = "Nurlan";
+
+// const surname = "Alizade";
+
+// surname = "Aliyev"
+
+// console.log(surname);
+
+
+let number = 5;
+
+let string = "okay";
+
+// let bool = true;
+
+// let big = 123912391923n;
+
+let nulltype = null;
+
+let undefinedd = undefined;
+
+
+// console.log(++number);
+// console.log(number);
+
+// console.log(typeof typeof number);
+// console.log(typeof typeof string);
+// console.log(typeof typeof bool);
+// console.log(typeof typeof nulltype);
+// console.log(typeof typeof undefinedd);
+
+let num1 = 5;
+
+let num2 = "6"
+
+let bool = true;
+
+// console.log(num1 <= num2);
+
+// if (num1 < num2 && !bool) {
+//     console.log("its okay");
+// }
+
+// if (num1 > num2 || !bool || num1 == 5 || num2 == "6") {
+//     console.log("its okay?");
+// }
+
+// let arr = [1, 5, 7, 21, "Nurxan", { firstname: "Hasan" }, true, false]
+
+// for (let index = 0; index < arr.length; index++) {
+//     console.log(arr[index]);
+
+// }
+
+// let jamal = {
+//     name: "Nurlan",
+//     age: 19
+// }
+
+// console.log(jamal.age, jamal.name);
+
+
+
+// switch (age) {
+//     case 15:
+//     case 16:
+//     case 17:
+//         console.log("get boyuyun gelsin");
+//         break;
+//     case 18:
+//         console.log("ozun gel");
+//         break;
+
+//     default:
+//         console.log("nurlan gelsin");
+//         break;
+// }
+
+const age = 15;
+
+// age = 15
+// try {
+//     age = 18;
+// } catch (err) {
+//     // throw "you cannot change const value"
+
+//     throw new Error("cannot change const value")
+//         // console.log(err);
+// } finally {
+//     console.log("this block does not care about any block");
+// }
+
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+// }
+
+// while (true) {
+//     console.log("okay");
+// }
+
+// for (;;) {
+//     console.log("okay");
+
+// }
